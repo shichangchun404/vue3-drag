9439e247c165b952c9b392ce25c21a857b48080c
shizhangchun <shichangchun@jd.com>
2022-09-25 08:31:42 +0800

    add 拖动事件

--- src/packages/editor.jsx
+++ src/packages/editor.jsx
@@ -1,4 +1,4 @@
-import { computed, defineComponent, inject } from "vue";
+import { computed, defineComponent, inject, ref } from "vue";
 import './editor.less'
 import EditorBlock from "./editor-block";
 
@@ -6,14 +6,16 @@ export default defineComponent({
   props: {
     modelValue: { type: Object}
   },
+  emits: ['update:modelValue'],
   setup(props, ctx){
     const data = computed({
       get(){
         return props.modelValue
+      },
+      set(newvalue){
+        ctx.emit('update:modelValue', JSON.parse(JSON.stringify(newvalue)))
       }
     })
-    
-
     const containerStyle = computed(()=>{
       return {
         width: data.value.container.width + 'px',
@@ -25,13 +27,59 @@ export default defineComponent({
     const config = inject('config')
     const componentList = config.componentList
 
+    const containerRef = ref(null)
+    let currentComponent = null
+
+    const dragenter = (e)=> {
+      // console.log('dragentter ...')
+      e.dataTransfer.dropEffect = 'move'
+    }
+    const dragover = (e)=> {
+      // console.log('dragmove ...')
+      e.preventDefault()
+      
+    }
+    const dragleave = (e)=> {
+      // console.log('dragleave ...')
+      e.dataTransfer.dropEffect = 'move'
+    }
+    const drop = (e)=> {
+      console.log('drop ...')
+      let blocks = data.value.blocks
+      data.value = { 
+        ...data.value, 
+        blocks: [
+          ...blocks,
+          {
+            top: e.offsetY,
+            left: e.offsetX,
+            zIndex: 1,
+            key: currentComponent.key
+          }
+        ],
+        
+
+      }
+    }
+    const dragStart = (e,component) => {
+      console.log('dragStart ', component);
+      containerRef.value.addEventListener('dragenter', dragenter) // 进入容器
+      containerRef.value.addEventListener('dragover', dragover) // 经过容器
+      containerRef.value.addEventListener('dragleave', dragleave) // // 离开容器
+      containerRef.value.addEventListener('drop', drop) // // 松手时 向容器里添加拖动的组件
+      currentComponent = component
+    }
     return ()=>{
       return <div class="editor-wrap">
+        {/* 左侧物料区域 可进行拖拽 */}
         <div class="editor-wrap-left">
           {
             componentList.map(component => {
               return (
-                <div class="left-block-item">
+                <div class="left-block-item" 
+                  draggable
+                  onDragstart={ e => dragStart(e, component) }
+                >
                   <span class="item-label">{component.label}</span>
                   <span class="item-component">{component.preview()}</span>
                 </div>
@@ -41,11 +89,12 @@ export default defineComponent({
           }
           
         </div>
+
         <div class="editor-wrap-mid">
           <div class="menu">菜单栏</div>
           <div class="container-wrap">
             <div class="container-layout">
-              <div class="container" style={containerStyle.value}>
+              <div class="container" style={containerStyle.value} ref={containerRef}>
                 {
                   data.value.blocks.map((block,index) => {
                     return <EditorBlock block={block}></EditorBlock>
