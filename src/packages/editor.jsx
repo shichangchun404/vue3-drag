15d5c12b4c892fab2b342de51e758ceb5475f1db
shizhangchun <shichangchun@jd.com>
2022-09-25 09:56:18 +0800

    add useFocus

--- src/packages/editor.jsx
+++ src/packages/editor.jsx
@@ -2,22 +2,23 @@ import { computed, defineComponent, inject, ref } from "vue";
 import './editor.less'
 import EditorBlock from "./editor-block";
 import useMenuDragger from './useMenuDragger'
+import useFocus from "./useFocus";
 
 export default defineComponent({
   props: {
-    modelValue: { type: Object}
+    modelValue: { type: Object }
   },
   emits: ['update:modelValue'],
-  setup(props, ctx){
+  setup(props, ctx) {
     const data = computed({
-      get(){
+      get() {
         return props.modelValue
       },
-      set(newvalue){
+      set(newvalue) {
         ctx.emit('update:modelValue', JSON.parse(JSON.stringify(newvalue)))
       }
     })
-    const containerStyle = computed(()=>{
+    const containerStyle = computed(() => {
       return {
         width: data.value.container.width + 'px',
         height: data.value.container.height + 'px'
@@ -27,39 +28,54 @@ export default defineComponent({
     const config = inject('config')
     const componentList = config.componentList
     const containerRef = ref(null)
-    const { dragStart, dragend} = useMenuDragger(data,containerRef)
 
-    
-    return ()=>{
+    // 菜单的拖拽
+    const { dragStart, dragend } = useMenuDragger(data, containerRef)
+
+    // 获取元素焦点
+    const { containerMouseDown, blockMouseDown } = useFocus(data)
+
+    return () => {
       return <div class="editor-wrap">
         {/* 左侧物料区域 可进行拖拽 */}
         <div class="editor-wrap-left">
           {
             componentList.map(component => {
               return (
-                <div class="left-block-item" 
+                <div
+                  class="left-block-item"
                   draggable
-                  onDragstart = { e => dragStart(e, component) }
-                  onDragend  = { e => dragend(e)}
+                  onDragstart={e => dragStart(e, component)}
+                  onDragend={e => dragend(e)}
                 >
                   <span class="item-label">{component.label}</span>
                   <span class="item-component">{component.preview()}</span>
                 </div>
               )
-               
+
             })
           }
-          
+
         </div>
 
         <div class="editor-wrap-mid">
-          <div class="menu">菜单栏</div>
+          <div class="menu">编辑工具栏</div>
           <div class="container-wrap">
             <div class="container-layout">
-              <div class="container" style={containerStyle.value} ref={containerRef}>
+              <div
+                class="container"
+                style={containerStyle.value}
+                ref={containerRef}
+                onMousedown={e => containerMouseDown(e)}
+              >
                 {
-                  data.value.blocks.map((block,index) => {
-                    return <EditorBlock block={block}></EditorBlock>
+                  data.value.blocks.map((block, index) => {
+                    return <EditorBlock
+                      block={block}
+                      class={block.focus ? 'block-focus' : ''}
+                      onMousedown={e => blockMouseDown(e, block)}
+
+                    ></EditorBlock>
                   })
                 }
               </div>
---
+++ src/packages/useFocus.js
@@ -0,0 +1,37 @@
+import { computed } from "vue"
+
+export default function useFocus(data){
+  const focusData = computed(() => {
+    let focus = []
+    let onfocused = []
+    data.value.blocks.forEach(block => (block.focus ? focus : onfocused).push(block))
+    return { focus, onfocused }
+  })
+  const containerMouseDown = () => { // 点击容器 失去所有焦点
+    clearBlocksFocus()
+  }
+  const clearBlocksFocus = () => { // 清空所有焦点
+    data.value.blocks.forEach(item => item.focus = false)
+  }
+
+  const blockMouseDown = (e, block) => {
+    e.preventDefault()
+    e.stopPropagation()
+    // 给元素对象添加一个选中标志 focus
+    if (e.shiftKey) {
+      block.focus = !block.focus
+    } else {
+      if (!block.focus) {
+        clearBlocksFocus()
+        block.focus = true
+      } else {
+        block.focus = false
+      }
+    }
+  }
+
+  return {
+    containerMouseDown,
+    blockMouseDown
+  }
+}
