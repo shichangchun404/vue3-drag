6a5a135649f0bac2bb803c3208402a8dbcee66e6
shizhangchun <shichangchun@jd.com>
2023-02-10 14:30:01 +0800

    feat(): add

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 2, // 0:关闭(off) 1:warn 2:error
+  },
 }
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import 'element-plus/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 
-const test = 123
-
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
